acf5c48fdd5a4c65ac481b6c5778fffa70b1d258
An Van Nguyen <152575252+anvng@users.noreply.github.com>
2024-06-16 18:39:22 +0700

    style: show new task and home

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.example.todolist
 
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.todolist.databinding.ActivityMainBinding
+import com.example.todolist.utils.setupDialog
 
 class MainActivity : AppCompatActivity() {
+    private val mainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val addTaskDialog: Dialog by lazy {
+        Dialog(this, R.style.DialogCustomTheme).apply {
+            setupDialog(R.layout.add_task_dialog)
+        }
+    }
+
+    private val updateTaskDialog: Dialog by lazy {
+        Dialog(this,R.style.DialogCustomTheme).apply {
+            setupDialog(R.layout.update_task_dialog)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(mainBinding.root)
+
+        //Close
+        val addCloseImg = addTaskDialog.findViewById<ImageView>(R.id.closeImg)
+        val updateCloseImg = updateTaskDialog.findViewById<ImageView>(R.id.closeImg)
+
+        addCloseImg.setOnClickListener {addTaskDialog.dismiss()}
+        updateCloseImg.setOnClickListener {updateTaskDialog.dismiss()}
+
+        mainBinding.addTaskNab.setOnClickListener {
+            addTaskDialog.show()
         }
     }
 }
