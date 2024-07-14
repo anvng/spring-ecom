67a5ea7573022616c9cd4950f555adafe2fc49a6
An Van Nguyen <152575252+anvng@users.noreply.github.com>
2024-07-14 12:34:20 +0700

    fix: mainactive func doesnt work and fix databinding

--- app/src/main/java/com/example/todolist/ui/MainActivity.kt
+++ app/src/main/java/com/example/todolist/ui/MainActivity.kt
@@ -1,6 +1,29 @@
 package com.example.todolist.ui
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.example.todolist.R
 
 class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment))
+//        val navController = findNavController(R.id.nav_host_fragment)
+//        setupActionBarWithNavController(navController)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navHostFragment.findNavController()
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
 }
