749c6e9d0f34fecc31c3c8657e183651a922d486
An Van Nguyen <152575252+anvng@users.noreply.github.com>
2024-07-25 16:10:54 +0700

    feat: delete task and all item

--- app/src/main/java/com/example/todolist/ui/task/TaskFragment.kt
+++ app/src/main/java/com/example/todolist/ui/task/TaskFragment.kt
@@ -1,15 +1,22 @@
 package com.example.todolist.ui.task
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.example.todolist.R
 import com.example.todolist.databinding.FragmentTaskBinding
 import com.example.todolist.viewmodel.TaskViewModel
+import com.google.android.material.snackbar.Snackbar
 
 class TaskFragment : Fragment() {
 
@@ -25,8 +32,13 @@ class TaskFragment : Fragment() {
 
         binding.lifecycleOwner = this
 
-        adapter = TaskAdapter(TaskClickListener {taskEntry ->
-            findNavController().navigate(TaskFragmentDirections.actionTaskFragmentToUpdateFragment(taskEntry))})
+        adapter = TaskAdapter(TaskClickListener { taskEntry ->
+            findNavController().navigate(
+                TaskFragmentDirections.actionTaskFragmentToUpdateFragment(
+                    taskEntry
+                )
+            )
+        })
 
         viewModel.getAllTasks.observe(viewLifecycleOwner) {
             adapter.submitList(it)
@@ -38,7 +50,55 @@ class TaskFragment : Fragment() {
                 findNavController().navigate(R.id.action_taskFragment_to_addFragment)
             }
         }
-        // Inflate the layout for this fragment
+
+        ItemTouchHelper(object :
+            ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val taskEntry = adapter.currentList[position]
+                viewModel.delete(taskEntry)
+
+                Snackbar.make(binding.root, "Task Deleted", Snackbar.LENGTH_LONG).apply {
+                    setAction("Undo") {
+                        viewModel.insert(taskEntry)
+                    }
+                    show()
+                }
+            }
+
+        }).attachToRecyclerView(binding.recyclerView)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.task_menu, menu)
+    }
+
+    private fun deleteAllItem() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Delete All?")
+            .setMessage("Are you sure you want to delete all tasks?")
+            .setPositiveButton("Yes") { _, _ ->
+                viewModel.deleteAll()
+            }.setNegativeButton("No") { _, _ ->
+            }.create().show()
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.btnDeleteAll -> deleteAllItem()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
